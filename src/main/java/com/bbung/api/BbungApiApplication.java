907d594e07ba0f36b957d7479668c6722cc480d0
jslee <kokonb0@efit.co.kr>
2022-11-22 13:55:22 +0900

    feat: base line

---
+++ src/main/java/com/bbung/api/BbungApiApplication.java
@@ -0,0 +1,13 @@
+package com.bbung.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbungApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbungApiApplication.class, args);
+    }
+
+}
